441ce7ac5c1b8a2e62c01ebca246df62cc4f82c2
swarup Goswami <goswamiswarup97@gmail.com>
2025-12-13 02:00:52 +0530

    added aprser

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from"dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+
+dotenv.config();
+const app=express();
+const port=process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/auth',authRoutes);
+
+
+app.listen(port,()=>
+{console.log("server is reunning on the port");
+connectDB();
+});
