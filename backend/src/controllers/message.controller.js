ca42a104c204485a61c36adc15a2baeb2a9e2a35
swarup Goswami <goswamiswarup97@gmail.com>
2025-12-13 07:49:07 +0530

    added the messageController

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,70 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
+
+export const getUserForSidebar=async(req,res)=>{
+    try {
+        const loggedInUserId=req.user._id;
+        // now ill filter that except this id (which is me) fetch all the id and since it is protected route i have access to req.user
+
+        const filteredUsers=await User.find({_id:{$ne:loggedInUserId}}).select('-password');
+
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.log("error",error);
+        res.status(500).json({error:"Internal server error"});
+    }
+}
+
+export const getMessages=async(req,res)=>{
+    try {
+        const {id:userToChatId}=req.params;
+        const myId=req.user._id;
+
+        const messages=await Message.find({
+            $or:[
+                {senderId:myId,reciverId:userToChatId},
+                {senderId:userToChatId,reciverId:myId}
+            ]
+        })
+
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("error",error);
+        res.status(500).json({error:"Internal server error"});
+    }
+}
+
+export const sendMessage=async(req,res)=>{
+    try {
+        const {text,image}=req.body;
+        const{id:reciverId}=req.params;
+        const senderId=req.user._id;
+
+        let imageUrl;
+        if(!image){
+            // upload base64 image to cloudinary
+            const uploadResponse=await cloudinary.uploader.upload(image);
+            imageUrl=uploadResponse.secure_url;
+        }
+
+        const newMessage=new Message({
+            senderId,
+            reciverId,
+            text,
+            image:imageUrl,
+        });
+
+        await newMessage.save();
+
+        // realtime functionality goes here => socket.io
+
+        res.status(201).json(newMessage);
+    } catch (error) {
+
+        console.log("error",error.message);
+        res.status(500).json({error:"Internal server error"});
+        
+    }
+}
