62d7ceaf585caab6057b5b259bd8a44d87367335
swarup Goswami <goswamiswarup97@gmail.com>
2025-12-13 07:53:19 +0530

    added the message route

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import {getUserForSidebar,getMessages,sendMessage} from "../controllers/message.controller.js";
+
+
+const router=express.Router();
+
+
+router.get('/users',protectRoute,getUserForSidebar);  
+router.get('/:id',protectRoute,getMessages);
+router.post('/send/:id',protectRoute,sendMessage);
+
+
+export default router;
