17e0f414d333fc67dacd1658931e799bee670fb0
swarup Goswami <goswamiswarup97@gmail.com>
2025-12-13 07:49:52 +0530

    updated index.js file

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from"dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import messageRoutes from "./routes/message.route.js";
 
 
 dotenv.config();
@@ -11,7 +12,10 @@ const port=process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+
+
 app.use('/api/auth',authRoutes);
+app.use('/api/message',messageRoutes);
 
 
 app.listen(port,()=>
