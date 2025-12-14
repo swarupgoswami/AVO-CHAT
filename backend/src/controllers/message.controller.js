546fd704e3049b87336f3262539e0fc5b785510c
swarup Goswami <goswamiswarup97@gmail.com>
2025-12-14 12:05:24 +0530

    updated message.conroller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -43,7 +43,7 @@ export const sendMessage=async(req,res)=>{
         const senderId=req.user._id;
 
         let imageUrl;
-        if(!image){
+        if(image){
             // upload base64 image to cloudinary
             const uploadResponse=await cloudinary.uploader.upload(image);
             imageUrl=uploadResponse.secure_url;
