34b6f7cb5070cbe19b80addb831a30d5bc16ebfe
swarup Goswami <goswamiswarup97@gmail.com>
2025-12-14 13:36:41 +0530

    updated the index.js file

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,11 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 
 dotenv.config();
-const app=express();
+
 const port=process.env.PORT || 5000;
 
 app.use(express.json({ limit: "10mb" }));
@@ -23,7 +24,7 @@ app.use('/api/auth',authRoutes);
 app.use('/api/message',messageRoutes);
 
 
-app.listen(port,()=>
+server.listen(port,()=>
 {console.log("server is reunning on the port");
 connectDB();
 });
